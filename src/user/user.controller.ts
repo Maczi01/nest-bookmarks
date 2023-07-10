6cc3b3e06edb0bac0305f1ea176c643c26f72760
maczi01 <mateusz.w.twardy@gmail.com>
2023-07-10 22:14:37 +0100

    feat: added edit user (with patch) in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, HttpCode, HttpStatus, Req, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { JwtGuard } from '../auth/guard';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('me')
   @HttpCode(HttpStatus.CREATED)
   getLoggedUserInfo(@GetUser() user: User) {
@@ -17,5 +30,10 @@ export class UserController {
   @Get()
   getUsers() {
     return [];
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
